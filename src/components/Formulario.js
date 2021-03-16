f816d92015a882e143c378e3864385421663467c
Armando <armandotechno12@gmail.com>
2021-03-16 15:26:36 -0400

    Agregando las citas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     // crear State de citas
     const [cita, actualizarCita] = useState({
@@ -42,6 +42,7 @@ const Formulario = () => {
         cita.id = uuidv4();
 
         // Crear la cita
+        crearCita(cita);
 
         // Reiniciar el form
     }
