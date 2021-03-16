a8d3887c9ee4793c4b343a3b4ca0213e446335dd
Armando <armandotechno12@gmail.com>
2021-03-16 17:31:01 -0400

    Agregando localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
 
+  // Citas en LocalStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales ) {
+    citasIniciales = [];
+  }
+
   // Arreglo de citas
   const [citas, guardarCitas] = useState([]);
 
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+      if(citasIniciales) {
+        localStorage.setItem('citas', JSON.stringify(citas))
+      } else {
+        localStorage.setItem('citas', JSON.stringify([]));
+      }
+  }, [citas] );
+
+  
   // Función que tome las citas actuales y agregue las nuevas
   const crearCita = cita => {
     guardarCitas([
