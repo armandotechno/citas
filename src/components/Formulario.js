078833621820db4f8f3208997e96499bc088281b
Armando <armandotechno12@gmail.com>
2021-03-16 15:29:59 -0400

    Reiniciando el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -45,6 +45,13 @@ const Formulario = ({crearCita}) => {
         crearCita(cita);
 
         // Reiniciar el form
+        actualizarCita({
+            mascota: '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        })
     }
 
     return (
