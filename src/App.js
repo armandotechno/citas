e2d71a0a5c81564c86d5bba5349350a859d9be4d
Armando <armandotechno12@gmail.com>
2021-03-16 17:11:29 -0400

    Agregando un mensaje de forma condicional

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ function App() {
     guardarCitas(nuevasCitas);
   }
 
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
 
   return (
    <Fragment>
@@ -34,7 +36,7 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita =>(
               <Cita
                 key={cita.id}
